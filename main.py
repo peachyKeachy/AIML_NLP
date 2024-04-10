9127ba6fb484dce5710ac7f23c9bc37d0bbc7bf4
peachyKeachy <156981435+peachyKeachy@users.noreply.github.com>
2024-04-10 19:26:15 +0000

    vectorize vocabulary

--- main.py
+++ main.py
@@ -7,9 +7,20 @@ sns.set_style("whitegrid")
 plt.style.use("fivethirtyeight")
 
 # example text for model training(SMS messasges)
+# (vocabulary)
 simple_train = ['call you tonight', 'call me a cab', 'please call me... PLEASE!']
 
+# fucntion from sklearn
 vect = CountVectorizer()
+
+# creates vocabulary
 vect.fit(simple_train)
+
+# prints unique words from vocabulary
 print(vect.get_feature_names_out())
+
+# transform training data/ vocabulary into a 'document-term matrix
+# computer will understand this way
+simple_train_dtm = vect.transform(simple_train)
+print(simple_train_dtm)
 
