1e55be6ff427ab4da4a3e67cda2f246021ac8339
peachyKeachy <156981435+peachyKeachy@users.noreply.github.com>
2024-04-10 19:08:28 +0000

    vocab created

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello")
+import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer
+
+sns.set_style("whitegrid")
+plt.style.use("fivethirtyeight")
+
+# example text for model training(SMS messasges)
+simple_train = ['call you tonight', 'call me a cab', 'please call me... PLEASE!']
+
+vect = CountVectorizer()
+vect.fit(simple_train)
+print(vect.get_feature_names_out())
+
